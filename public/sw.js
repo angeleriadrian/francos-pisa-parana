131181a2d170b430ccaad8b8030bb5113848e1f5
angeleriadrian <angeleriadrian@gmail.com>
2026-08-26 15:18:45 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'francos-pisa-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Solo interceptamos navegación (HTML) para detectar nuevas versiones
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/index.html'))
+    );
+  }
+});
